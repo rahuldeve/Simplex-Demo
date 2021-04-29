d8815e0bb21ea3f66f45674ac4e2aca3cf952fba
Rahul Dev <rahul.e.dev@gmail.com>
2021-04-29 23:23:11 +0530

    Update main.py file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from parse import parse_linear_program
-from lp import standardize
+from parse import *
+from utils import *
+from convert import *
+from solve import *
 
 text = '''
 var x1 >= 0;
-var x2 <= 0;
+var x2 >= 0;
 
 st:   x1 +   x2 <=  9;
 st: 3x1 +   x2 <= 18;
@@ -13,4 +15,8 @@ st:          x2 >=  6;
 maximize:     3x1 + 2x2;
 '''
 
-prog = parse_linear_program(text)
+p = parse_linear_program(text)
+q = to_standardard_form(p)
+q = to_slack_form(q)
+print(q.variables.keys())
+print(simplex(q))
